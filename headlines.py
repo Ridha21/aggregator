82a5255441bb1b81fe84817ace346946ae5e68a3
Ridha21 <58891036+Ridha21@users.noreply.github.com>
2019-12-17 14:55:13 +0100

    Delete headlines.py

--- headlines.py
+++
@@ -1,9 +0,0 @@
-import pprint
-import requests
-secret ='82350f6927f74fca8c3cb5d60e604954'
-url = 'https://newsapi.org/v2/top-headlines?'
-parameters = {
-    'q' : 'news',
-    'pageSize' : 10,
-    'apiKey' : secret
-}
